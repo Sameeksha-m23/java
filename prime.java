ffa28777713432105c18b37981b435b60145466d
Sameeksha <mutagekarsameeksha@gmail.com>
2026-06-08 22:19:54 +0530

    B

--- prime.java
+++ prime.java
@@ -2,5 +2,7 @@ import java.util.*;
 public class prime {
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter the number");
+    System.out.println("Enter the number");
+
     
 }
